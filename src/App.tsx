2c4cea6e375e5cea1121e31f37267ef7060b1c5b
alexis-guerquin <alexis.guerquin@gmail.com>
2025-06-17 23:11:39 +0200

    Mise à jour des dépendances et ajout de styles pour le composant DailyQuest. Création d'un fichier de configuration pour les tests avec @testing-library/jest-dom et déclaration des modules CSS dans vite-env.d.ts.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import './App.css'
-function App() {
+import './App.css';
+import DailyQuest from './components/Quests/DailyQuest/DailyQuest';
+import { QuestProvider } from './contexts/QuestContext';
+import ComponentTester from './components/Quests/ComponentTester/ComponentTester';
 
+function App() {
   return (
-    <>
-    </>
-  )
+    <div className="app">
+      <QuestProvider>
+        <ComponentTester />
+        <div className="daily-missions">
+          <DailyQuest
+            style="pomodoro"
+          />
+        </div>
+      </QuestProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;
