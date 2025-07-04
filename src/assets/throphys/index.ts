2885a2429e06ddff6ca3f3a3f138b7a9b60c5ea7
alexis-guerquin <alexis.guerquin@gmail.com>
2025-07-04 22:32:19 +0200

    trophys index

---
+++ src/assets/throphys/index.ts
@@ -0,0 +1,39 @@
+// Trophées Tasks
+export { default as tasksBronze } from './tasks/bronze.svg';
+export { default as tasksIron } from './tasks/iron.svg';
+export { default as tasksSilver } from './tasks/silver.svg';
+export { default as tasksGold } from './tasks/gold.svg';
+export { default as tasksUnlocked } from './tasks/unlocked.svg';
+export { default as tasksDefault } from './tasks/default.svg';
+
+// Trophées Streak
+export { default as streakBronze } from './streak/bronze.svg';
+export { default as streakIron } from './streak/iron.svg';
+export { default as streakSilver } from './streak/silver.svg';
+export { default as streakGold } from './streak/gold.svg';
+export { default as streakUnlocked } from './streak/unlocked.svg';
+export { default as streakDefault } from './streak/default.svg';
+
+// Trophées Daily
+export { default as dailyBronze } from './daily/bronze.svg';
+export { default as dailyIron } from './daily/iron.svg';
+export { default as dailySilver } from './daily/silver.svg';
+export { default as dailyGold } from './daily/gold.svg';
+export { default as dailyUnlocked } from './daily/unlocked.svg';
+export { default as dailyDefault } from './daily/default.svg';
+
+// Trophées Clock
+export { default as clockBronze } from './clock/bronze.svg';
+export { default as clockIron } from './clock/iron.svg';
+export { default as clockSilver } from './clock/silver.svg';
+export { default as clockGold } from './clock/gold.svg';
+export { default as clockUnlocked } from './clock/unlocked.svg';
+export { default as clockDefault } from './clock/default.svg';
+
+// Trophées Grind
+export { default as grindBronze } from './grind/bronze.svg';
+export { default as grindIron } from './grind/iron.svg';
+export { default as grindSilver } from './grind/silver.svg';
+export { default as grindGold } from './grind/gold.svg';
+export { default as grindUnlocked } from './grind/unlocked.svg';
+export { default as grindDefault } from './grind/default.svg'; 
